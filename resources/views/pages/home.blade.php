ea1568678780a2a388a9ba861ee83a8c7d1ce1cc
arga <it.wsm2026@gmail.com>
2026-07-04 07:47:12 +0700

    starter setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('template.layout')
+@section('content')
+    <div class="flex flex-col justify-center items-center w-full h-screen">
+        <h1 class="text-white text-2xl font-semibold">Welcome to Mavnus</h1>
+        <h1 class="text-white text-xl">This is the home page.</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/')->group(function () {
+    Route::get('/', [homeController::class, 'home'])->name('home');
 });
