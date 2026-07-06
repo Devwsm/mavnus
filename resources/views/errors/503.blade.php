67a5889743c2af7e0f0a7cb7174ecb06c896b93b
arga <it.wsm2026@gmail.com>
2026-07-06 13:10:55 +0700

    w 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,6 +2,6 @@
 @section('content')
     <div class="min-h-screen flex text-white items-center justify-center text-center">
         <img src="{{ asset('aset/mavnus-maintenance.png') }}" loading="lazy" decoding="async" alt="mavnus"
-            class="object-cover object-center w-120 rounded-full">
+            class="object-cover object-center w-100 rounded-full">
     </div>
 @endsection
